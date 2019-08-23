00bf288c99515e298cac04ea4002e7f71b2e47b2
Manish <kmanish75@gmail.com>
2019-08-23 13:32:04 -0500

    Commit to Repo

---
+++ src/main/java/com/trilogyed/taskercloudconfigserver/TaskerCloudConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.trilogyed.taskercloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class TaskerCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskerCloudConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trilogyed/taskercloudconfigserver/TaskerCloudConfigServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.trilogyed.taskercloudconfigserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaskerCloudConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
